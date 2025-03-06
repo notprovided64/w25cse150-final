387c4c77f62820dec9f5919fe41d8f6dfc90c67a
notpr <notprovided64@gmail.com>
2025-03-05 18:25:15 -0800

    almost there on client

--- pclaytonClient.py
+++ pclaytonClient.py
@@ -3,9 +3,10 @@ import argparse, socket, select, signal, sys, os, re
 BUFSIZE = 4096
 INV_IN_MSG = "invalid input provided"
 
-TIMEOUT = 10 # in seconds
+TIMEOUT = 30 # in seconds
 
 bridgeack_regex = r"^BRIDGEACK\r\nclientID: *(\S*)\r\nIP: *([\d\.]*)\r\nPort: *(\d*)\r\n\r\n$"
+chat_regex = r"^CHAT\r\nclientID: *(\S*)\r\nIP: *([\d\.]*)\r\nPort: *(\d*)\r\n\r\n$"
 
 class ConnectionInfo:
     def __init__(self, id:str, host:str, port:int):
@@ -57,7 +58,6 @@ def validate_args(args):
 
     return True
 
-
 def parse_args():
     parser = argparse.ArgumentParser(description="client for final project")
     
@@ -74,7 +74,7 @@ def send(host, port, contents):
     client_socket.settimeout(TIMEOUT)
 
     try:
-        client_socket.connect((host, port))
+        client_socket.connect((host, int(port)))
         client_socket.send(contents.encode())
 
         data = client_socket.recv(BUFSIZE)
@@ -112,65 +112,101 @@ def parse_bridgeack(msg):
 
     return None
 
-def chat(args, connection_info):
-    serv_host, serv_port = args.server.split(':')
+def wait(args):
 
     s = None
     try:
-        if wait == True: # setup server socket to handle incoming connection
-            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server_socket.bind((serv_host, serv_port))
-            server_socket.settimeout(TIMEOUT)
-            server_socket.listen(1)
-            s, _ = server_socket.accept()
-        else:            # otherwise we try to connect to connection_info 
-            id, host, port = connection_info
-            port = int(port)
-            # separate connection_info class would be best here, validation should be integrated into the data type
+        serv_host = socket.gethostbyname("localhost")
+        serv_port = args.port
 
-            print(f"now chatting with {id}")
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, int(port)))
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((serv_host, int(serv_port)))
+        server_socket.settimeout(TIMEOUT * 10)
+        server_socket.listen(1)
+        s, s_addr = server_socket.accept()
 
-            s.send("".encode()) # put chat request here if needed
+        data = s.recv(BUFSIZE) #hopefully should be a /CHAT
+        if data is None:
+            raise Exception()
+        msg = data.decode()
+        match = re.search(chat_regex, msg)
+        if not match:
+            raise Exception("invalid chat message recieved")
 
-        while True:
-            uin = input()
-            if uin == "/quit":
-                s.send("QUIT".encode())
-                raise KeyboardInterrupt # want same behavior anyways
-            s.send(uin.encode())
+        id = match.group(1)
+        ip = match.group(2)
+        port = match.group(3)
 
-            data = s.recv(BUFSIZE)
-            if not data:
-                print("peer ended chat?")
-                raise KeyboardInterrupt
+        # can delete
+        client_ip, client_port = s_addr
+        print(f"socket info: {client_ip=}{client_port=}")
 
-            msg = data.decode("utf-8")
-            if msg == "QUIT":
-                print("peer ended chat")
-                raise KeyboardInterrupt
-            print(msg)
+        print(f"Incoming chat request from {id} {ip}:{port}")
+        chat_loop(s, False)
 
-        s.close()
-
-        return msg
-        
     except KeyboardInterrupt:
         if s is not None:
             s.close()
         exit()
     except Exception as e:
-        print(f"error occurred while chatting: {e}")
+        print(f"error occurred while waiting: {e}")
         if s is not None:
             s.close()
 
-def chatLoop():
+def chat_init(args, info):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.settimeout(TIMEOUT)
+
+    try:
+        localhost = socket.gethostbyname("localhost")
+        port = args.port
+
+        print(f"shouldn't exist{info.host}")
+        client_socket.connect((info.host, info.port))
+        client_socket.send(f"CHAT\r\nclientID: {args.id}\r\nIP: {localhost}\r\nPort: {port}\r\n\r\n".encode())
+        #parse input message on s, if it's "CHAT", then call loop
+        chat_loop(client_socket, True)
+
+    except KeyboardInterrupt:
+        client_socket.close()
+        exit()
+    except Exception as e:
+        print(f"error occurred while chatting: {e}")
+        client_socket.close()
+
+
+def chat_loop(s:socket.SocketType, is_writing:bool):
+    try:
+        while True:
+            if is_writing:
+                uin = input()
+                if uin == "/quit":
+                    raise KeyboardInterrupt # want same behavior anyways
+                s.send(uin.encode())
+                is_writing = False
+            else:
+                data = s.recv(BUFSIZE)
+                if not data:
+                    print("peer ended chat?")
+                    raise KeyboardInterrupt
+
+                msg = data.decode("utf-8")
+                if msg == "QUIT":
+                    print("peer ended chat")
+                    raise KeyboardInterrupt
+                print(msg)
+                is_writing = True
+                
+    except KeyboardInterrupt:
+        s.send("QUIT".encode())
+        exit()        
+
 
 def loop(args):
     host, port = args.server.split(':')
     client_ip = socket.gethostbyname(socket.gethostname())
     print(f"{args.id} running on {client_ip}:{port}")
+    bridge_info = None
 
     try:
         while(True):
@@ -184,12 +220,20 @@ def loop(args):
                 send(host, port, get_reg_req(args.id, args.port))
             elif uin == "/bridge":
                 msg = send(host, port, get_bridge_req(args.id))
-            elif uin == "/chat":
                 connection_info = parse_bridgeack(msg)
-                if connection_info is not None:
-                    print(connection_info)
-                    chat(args, connection_info, connection_info.is_empty())
-                    exit()
+                print(connection_info.host, connection_info.port)
+
+                if connection_info.is_empty():
+                    wait(args)
+                    continue
+
+                bridge_info = connection_info
+            elif uin == "/chat":        
+                if not bridge_info:
+                    print("you don't have anything saved")
+                    continue
+
+                chat_init(args, bridge_info)
 
             else:
                 print(INV_IN_MSG)
